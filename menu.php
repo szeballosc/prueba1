4b9dce12c668eac3127c5dde13d2414f3c4c829a
szeballosc <szeballosc@gmail.com>
2018-05-16 00:33:53 -0400

    1

--- menu.php
+++ menu.php
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    include "funciones.php";
+    ?>
+    
+  </body>
+</html>
