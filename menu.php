21f0dc8d7bdf8b161ee55cb71f0aa513feebb765
szeballosc <szeballosc@gmail.com>
2018-05-16 00:54:00 -0400

    1

--- menu.php
+++ menu.php
@@ -8,6 +8,20 @@
     <?php
     include "funciones.php";
     ?>
-    
+    <h1>SUSHIOESTE</h1>
+    <select name="cmbProductos">
+      <option value="1">CALIFORNIA</option>
+      <option value="2">ENVUELTOS EN PALTA</option>
+      <option value="3">ENVUELTOS EN ATUN ROJO Y BLANCO</option>
+    </select>
+    <div class="">
+      
+    </div>
+    <div class="">
+
+    </div>
+    <div class="">
+
+    </div>
   </body>
 </html>
